637aa067ddaf96d016ad407f5b7c3c14869874ba
N-drakhman <fdrakhman@gmail.com>
2024-08-31 00:20:51 +0500

    changed order so FizzBuzz will printed instead of Fizz or Buzz when number is divided by 15

--- FizzBuzz/fizzBuzz.js
+++ FizzBuzz/fizzBuzz.js
@@ -1,12 +1,12 @@
 const inputNumber = parseInt(prompt("enter your number"));
 
 for (let i = 1; i <= inputNumber; i++) {
-  if (i % 3 == 0) {
+  if (i % 15 == 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 == 0) {
     console.log("Fizz ");
   } else if (i % 5 == 0) {
     console.log("Buzz");
-  } else if (i % 15 == 0) {
-    console.log("FizzBuzz");
   } else {
     console.log(i);
   }
