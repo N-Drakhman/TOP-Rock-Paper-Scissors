ad28cfbb0075cb08f2e3b09aef5329bfa2c1cc87
N-drakhman <fdrakhman@gmail.com>
2024-08-31 00:09:31 +0500

    added fizz-buzz for numbers divided by 15 with no remainder

--- FizzBuzz/fizzBuzz.js
+++ FizzBuzz/fizzBuzz.js
@@ -1,12 +1,13 @@
-const inputNumber = prompt("enter your number")
+const inputNumber = prompt("enter your number");
 
 for (let i = 1; i <= inputNumber; i++) {
   if (i % 3 == 0) {
     console.log("Fizz ");
   } else if (i % 5 == 0) {
     console.log("Buzz");
+  } else if (i % 15 == 0) {
+    console.log("FizzBuzz");
   } else {
     console.log(i);
   }
 }
-
